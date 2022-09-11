5f1dd5de0e33014612db3b15f44e523806e71f1d
Ryan Kaszubski <gn2289@wayne.edu>
2022-09-11 16:08:21 -0400

    reinstall and fix react and nx

--- front-end/ltaq/apps/ltaq/src/app/app.spec.tsx
+++ front-end/ltaq/apps/ltaq/src/app/app.spec.tsx
@@ -12,6 +12,6 @@ describe('App', () => {
   it('should have a greeting as the title', () => {
     const { getByText } = render(<App />);
 
-    expect(getByText(/Welcome long-term-air-quality-index/gi)).toBeTruthy();
+    expect(getByText(/Welcome ltaq/gi)).toBeTruthy();
   });
 });
--- front-end/ltaq/apps/ltaq/src/app/app.tsx
+++ front-end/ltaq/apps/ltaq/src/app/app.tsx
@@ -1,11 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.scss';
+import styles from './app.module.css';
 import NxWelcome from './nx-welcome';
 
 export function App() {
   return (
     <>
-      <NxWelcome title="long-term-air-quality-index" />
+      <NxWelcome title="ltaq" />
       <div />
     </>
   );
