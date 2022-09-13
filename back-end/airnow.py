047aecf7f18f1b9da7223c97c7dc3dfdcd62f076
Ryan K <47284437+ryanksz@users.noreply.github.com>
2022-09-12 21:07:23 -0400

    add more parameters to endpoint

--- back-end/airnow.py
+++ back-end/airnow.py
@@ -6,21 +6,29 @@ import json
 # API is in secrets
 # Do not push API_KEY to GitHub
 API_KEY = ""
-
+app = Flask(__name__)
+CORS(app)
 # Contract: https://docs.airnowapi.org/CurrentObservationsByZip/docs
 # Requires a valid zip code
 # Returns a zip code's current AQI in json
-def getCurrentAQIValuesByZipCode(zipcode):
+@app.route("/getCurrentAQIValuesByZipCode/<zipcode>")
+def getCurrentAQIValuesByZipCode(zipCode):
     getCurrentAQIValuesByZipCodeURL = "https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode="+zipCode+"&distance=25&API_KEY="+API_KEY
     getCurrentAQIValuesByZipCodeRequest = requests.get(getCurrentAQIValuesByZipCodeURL)
     return json.dumps(getCurrentAQIValuesByZipCodeRequest)
     
-
-def getAllData(minX, minY, maxX, maxY):
+# Contract: https://docs.airnowapi.org/Data/docs
+# Returns Ozone, PM25, PM10, CO, N02 and S02 Values for a location
+# Sample bounds: -124.205070,28.716781,-75.337882,45.419415
+# Sample date: 2022-09-12T00
+# Will return in json format 
+@app.route("/getAllData/<minX>,<minY>,<maxX>,<maxY>,<startdate>,<enddate>")
+def getAllData(minX, minY, maxX, maxY, startDate, endDate):
     getAllDataURL = """
-    https://www.airnowapi.org/aq/data/?startDate=2022-09-11T01&endDate=2022-09-11T02&parameters=PM25&
+    https://www.airnowapi.org/aq/data/?startDate="""+startDate+"""&endDate="""+endDate+"""
+    &parameters=OZONE,PM25,PM10,CO,NO2,SO2
     BBOX="""+minX+","+minY+","+maxX+","+maxY+","+"""
-    &dataType=A&format=text/csv&verbose=0&monitorType=0&includerawconcentrations=0&
+    &dataType=B&format=application/json&verbose=0&monitorType=2&includerawconcentrations=0&
     API_KEY="""+API_KEY
     getAllDataRequest = requests.get(getAllDataURL)
     return json.dumps(getAllDataRequest)
