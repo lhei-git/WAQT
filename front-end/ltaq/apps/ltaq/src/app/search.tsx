861c85797c9faca66a9934b5fcee9beae09f2033
kevinKluka <Kevinmkluka@gmail.com>
2022-09-17 11:00:17 -0400

    add files

---
+++ front-end/ltaq/apps/ltaq/src/app/search.tsx
@@ -0,0 +1,18 @@
+
+const SearchBar = () => (
+    <form action="http://www.google.com" method="get">
+        <label htmlFor="header-search">
+            <span className="visually-hidden">Search</span>
+        </label>
+        <input
+            type="text"
+            id="header-search"
+            placeholder="Location"
+        />
+        <button
+            type="submit">Submit
+        </button>
+    </form>
+);
+
+export default SearchBar;
