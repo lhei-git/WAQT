ae3b344e304c6d80a829502e90513daeecacf61a
Ryan Kaszubski <gn2289@wayne.edu>
2022-09-19 11:32:32 -0400

    small wildfire endpoint fix

--- back-end/main.py
+++ back-end/main.py
@@ -1,16 +1,8 @@
-#Example code only
-#Do not use in final product
-#!/usr/bin/env python3
-"""
-Documentation
-
-See also https://www.python-boilerplate.com/flask
-"""
-import os
-
+import requests, os, json, urllib.request, ssl
 from flask import Flask, jsonify
 from flask_cors import CORS
 
+ssl._create_default_https_context = ssl._create_unverified_context
 
 def create_app(config=None):
     app = Flask(__name__)
@@ -25,13 +17,19 @@ def create_app(config=None):
 
     # Definition of the routes. Put them into their own file. See also
     # Flask Blueprints: http://flask.pocoo.org/docs/latest/blueprints
-    @app.route("/")
-    def hello_world():
-        return "Hello World"
+    @app.route("/", methods=['GET'])
+    def WildFire():
+       with urllib.request.urlopen("https://services3.arcgis.com/T4QMspbfLg3qTGWY/arcgis/rest/services/Fire_History_Locations_Public/FeatureServer/0/query?where=1%3D1&outFields=CalculatedAcres,IncidentName&outSR=4326&f=json") as url:
+        res = json.load(url)
+        print(res)
+       return res
 
-    @app.route("/foo/<someId>")
-    def foo_url_arg(someId):
-        return jsonify({"echo": someId})
+    #To be updated once we have more specific data parameters
+    def WildFireData(IncidentName, CalculatedAcres):
+        options = {}
+        options["url"] = "https://services3.arcgis.com/T4QMspbfLg3qTGWY/arcgis/rest/services/Fire_History_Locations_Public/FeatureServer/0/query?where=1%3D1&outFields=CalculatedAcres,IncidentName&outSR=4326&f=json"
+        options["Incident_Name"] = IncidentName
+        options["Calculated_Acres"] = CalculatedAcres
 
     return app
 
