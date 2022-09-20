216239645bef1eac60a8541e5883b43bba94a9b8
Ryan Kaszubski <gn2289@wayne.edu>
2022-09-20 17:23:39 -0400

    initial commit with sample tables/graphs

--- front-end/ltaq/apps/ltaq/src/app/app.tsx
+++ front-end/ltaq/apps/ltaq/src/app/app.tsx
@@ -1,11 +1,15 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
-import NxWelcome from './nx-welcome';
+import Graphs from './graph';
+import LineGraph from './line'
+import Table from './current-table'
 
 export function App() {
   return (
     <>
-      <NxWelcome title="ltaq" />
+      <Graphs />
+      <LineGraph />
+      <Table />
       <div />
     </>
   );
