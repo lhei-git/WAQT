109023e0d5cf475952c4816830f05d8405e1792b
kevinKluka <Kevinmkluka@gmail.com>
2022-09-25 20:52:40 -0400

    front end working as expected

--- front-end/ltaq/apps/ltaq/src/app/searchLocation.tsx
+++ front-end/ltaq/apps/ltaq/src/app/searchLocation.tsx
@@ -1,79 +1,62 @@
-import React from 'react';
-import { useState } from 'react';
-import './app.css';
+import React, { useState } from 'react';
+import './App.css';
 
-const [selectedOption, setSelectedOption] = useState<String>();
+function searchLocation() {
 
-class Location extends React.Component<{}, { value: string }> {
+  //Array to hold user input 'location' and 'locationType'
+  const [state, setState] = useState({
+    location: '',
+    locationType: '',
+  });
 
-  constructor(props: {} | Readonly<{}>) {
-
-    super(props);
-    this.state = {
-      value: 'location',
-    };
-
-    this.locationChange = this.locationChange.bind(this); //User entered location handler
-    this.handleSubmit = this.handleSubmit.bind(this); //submit button handler
-  }
-
-  //Form Events
-  locationChange(event: { target: { value: any } }) {
-    localStorage.setItem("location", JSON.stringify({ value: event.target.value })); //Save user input in localStorage variable "location"
-    this.setState({ value: event.target.value });
-  }
-
-  handleSubmit(event: { preventDefault: () => void }) {
-    // alert('Location Entered: ' + this.state.value);
-    // alert('Location Entered: ' + localStorage.getItem('location'));
-    console.log(JSON.stringify(localStorage.getItem("location"))) //Get and pring localStorage varabile "location" and print to console.log
-    event.preventDefault();
-  }
-
-  // This function is triggered when the select changes
-  placeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    const value = event.target.value;
-    setSelectedOption(value);
+  //Any time a chage is made to X variable 'name', update the state array with the 'value'
+  const handleChange = (e: { target: { name: any; value: any } }) => {
+    setState({
+      ...state, //Spread syntax, allowing multiple arguments because 'state' is an array
+      [e.target.name]: e.target.value,
+    });
   };
-  
-  override render() {
-    
-    return (
-      <>
-        <div>
-          {/* <h1 className = "form-box h1">
-        Enter Location
-      </h1> */}
 
-          <form onSubmit={this.handleSubmit}>
-            <label>
-              Location:
-              {/* <textarea
-                value={this.state.value}
-                onChange={this.locationChange}
-              /> */}
-              <input
-                type="text"
-                // name="note"
-                value={this.state.value}
-                // onChange={this.locationChange}
-                onChange={this.locationChange}
-              />
-            </label>
-            <select onChange={this.placeChange}>
-              <option value="grapefruit">City</option>
-              <option value="lime">State</option>
-              <option selected value="coconut">
-                County
-              </option>
-              <option value="mango">Zip Code</option>
-            </select>
-            <input type="submit" value="Submit" />
-          </form>
-        </div>
-      </>
-    );
-  }
+  //Called when user hits 'submit' button
+  const handleSubmit = (event: { preventDefault: () => void; }) => {
+    event.preventDefault();
+    //Store state array in local storage and print to console
+    localStorage.setItem("LocationInfo", JSON.stringify(state));
+    console.log(state.location);
+    console.log(state.locationType);
+    }
+
+  return (
+    <div>
+      <h1>Wildfire Air Quality (WAQ)</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Location:{' '}
+          <input
+            type="text"
+            name="location"
+            value={state.location}
+            onChange={handleChange}
+          />
+        </label>{' '}
+        <label>
+          <select
+            name="locationType"
+            value={state.locationType}
+            onChange={handleChange}>
+            <option value="Default"></option>
+            <option value="City">City</option>
+            <option value="State">State</option>
+            <option selected value="County">County</option>
+            <option value="Zip Code">Zip Code</option>
+          </select>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+      <h5>Location: {state.location}</h5>
+      <h5>Type: {state.locationType}</h5>
+    </div>
+  );
 }
 
-export default Location;
+export default searchLocation;
