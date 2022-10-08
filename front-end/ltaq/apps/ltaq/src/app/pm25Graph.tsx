5e3ddec828e56d2e0a11fe081bd84b67a657b7a7
Ryan Kaszubski <gn2289@wayne.edu>
2022-10-08 17:51:32 -0400

    aqi graphs

---
+++ front-end/ltaq/apps/ltaq/src/app/pm25Graph.tsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react';
+import { Bar, Line } from 'react-chartjs-2';
+import { Chart, registerables } from 'chart.js';
+import axios from 'axios';
+import styles from "./app.module.css";
+Chart.register(...registerables);
+
+//returns a bar graph which displays PM25 Data
+//This data can be in a specific date range
+
+//TODO: Add coordinate support
+export default function PM25Graph() {
+  const bbox = '-83.553673,42.029418,-82.871707,42.451216';
+  const url =
+    'http://localhost:8000/pm25?bbox='+bbox;
+  console.log(url);
+  const [data, setData] = useState<any[]>([]);
+
+  useEffect(() => {
+    axios.get(url).then((response) => setData(response.data));
+  }, []);
+  return (
+    <div className={styles["graph"]}>
+      <Line
+        data={{
+          labels: Object.keys(data),
+          datasets: [
+            {
+              label: 'PM 25',
+              data: Object.values(data),
+            },
+          ],
+        }}
+        options={{
+          responsive: true,
+          plugins: {
+            legend: {
+              display: true,
+              labels: {
+                color: 'rgb(255, 99, 132)',
+              },
+            },
+            title: {
+              display: true,
+              text: 'PM 25 Chart',
+            },
+          },
+        }}
+      />
+    </div>
+  );
+}
