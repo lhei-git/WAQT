e5043fbd7ee56624e8613dda41f248d8625fc7a3
unknown <kevinmkluka@gmail.com>
2022-10-23 15:23:33 -0400

    Home Page and About

--- front-end/ltaq/apps/ltaq/src/app/app.tsx
+++ front-end/ltaq/apps/ltaq/src/app/app.tsx
@@ -1,23 +1,39 @@
-import { useLoadScript} from "@react-google-maps/api";
-import "./app.css";
+import { useLoadScript } from '@react-google-maps/api';
+import './app.css';
 import HOME from './home';
+import ABOUT from './about';
+import Nav from './nav';
 
 //This renders the search and greeting page of our web app
 const App = () => {
-    const { isLoaded } = useLoadScript({
-        googleMapsApiKey: 'KEY_HERE',
-        libraries: ["places"],
-    });
-    
-    if (!isLoaded) return <div>Loading...</div>
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: 'KEY_HERE',
+    libraries: ['places'],
+  });
 
-    return (
-        <>
-        {/* <Location /> */}
-        <HOME />
-        {/* <Places /> */}
-        </>
-    );
-}
+  if (!isLoaded) return <div>Loading...</div>;
+  let Component;
+  switch (window.location.pathname) {
+    case '/':
+      Component = HOME;
+      break;
+    case '/home':
+      Component = HOME;
+      break;
+    case '/about':
+      Component = ABOUT;
+      break;
+  }
+
+  return (
+    <>
+      {/* <Location /> */}
+      <Nav />
+      {/* <HOME /> */}
+      <Component />
+      {/* <Places /> */}
+    </>
+  );
+};
 
 export default App;
