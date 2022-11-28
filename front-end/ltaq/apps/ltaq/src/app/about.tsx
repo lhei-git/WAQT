c3105b9f6ac1b47143cb550950e297d3106c2302
ahmadaoun30 <98710478+ahmadaoun30@users.noreply.github.com>
2022-11-28 11:13:54 -0500

    about page fixes

--- front-end/ltaq/apps/ltaq/src/app/about.tsx
+++ front-end/ltaq/apps/ltaq/src/app/about.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import styles from './nav.module.css';
 import * as data from './links.json';
 import aboutStyle from './about.module.css';
-import TwitterIcon from '@mui/icons-material/Twitter';
+import ozoneValue from "./ozone.jpg"; 
+import pm25Value from "./pm25.jpg"; 
+import pm10Value from "./pm10.jpg"; 
+import FireTable from "./fireTable.jpg"; 
 //import Nav from './navAbout';
 
 const About = () => {
@@ -12,9 +15,9 @@ const About = () => {
       <h2 className={aboutStyle['title']}></h2>
       <body className={aboutStyle['body']}>
         <br />
-        <p>
-          <div>
-          <h1>About WAQT</h1>
+        <p>        
+          
+        <h1> About WAQT</h1>
           <br/>
           <p>One of the major silent killers and reducers of quality of life in
             the world today is air pollution. The European Environmental Agency
@@ -25,6 +28,7 @@ const About = () => {
             are<a href="https://www.preventionweb.net/files/73797_wildfiresbriefingnote.pdf"> exacerbating air pollution around the world.</a>
           </p>
           <br />
+
           <p>
           While many applications exist to report current levels of air pollutants, 
           these applications do not provide historical trends of pollution and air 
@@ -35,12 +39,14 @@ const About = () => {
           not enable visualization of air quality trends in the previous 5 or 7 years.
           </p>
           <br />
+
           <p>Although wildfires have become a serious concern, particularly in the United States 
           (US), where wildfire burned 4 million acres in 2020 in California. However, few current 
           applications exist to visualize current active wildfires and historic information about 
           wildfires in the US.
           </p>
           <br />
+
           <p>
           The Wildfire & Air Quality Tracker (WAQT) app was developed to provide citizens, scientists, 
           policy makers and the public alike the ability to easily search for air quality and wildfire 
@@ -50,6 +56,7 @@ const About = () => {
           application programming interface (API) of the US Environmental Protection Agency (EPA).  
           </p>
           <br />
+
           <p>
           WAQT also retrieves the current active wildfires in the searched location from the [XYZ] API, 
           of the National Interagency Fire Center, and various calculations of historical wildfire data 
@@ -57,6 +64,7 @@ const About = () => {
           API</a> of the National Interagency Fire Center. 
           </p>
           <br />
+
           <p> 
           The WAQT app also produces various graphs of the historical trends of the three major pollutants 
           identified from the year 2015 until the most recent data available. This long-term air quality 
@@ -65,22 +73,23 @@ const About = () => {
           wildfires—when the data is available. Further details about wildfire data calculations are provided below.
           </p>
           <br /> 
+
           <p>
           WAQT was conceptualized by Nic DePaula, who guided the development process. WAQT was developed by 
           Ryan Kaszubski, Ahmad Aoun and Kevin Kluka during the Fall of 2022 for their senior capstone project. 
           </p>
           <br/>
-          </div>
-
-          
+         
           <div>
           <h1>The Major Pollutants</h1>
           <br/>
+
           <p>
           EPA sets national air quality standards for 6 common pollutants, 3 of which are measured 
           by WAQT given their prominence in issues of air quality:
           </p>
           <br />
+          
           <ul>
           <li><a href="https://www.epa.gov/ground-level-ozone-pollution">Ozone (O3)</a></li>
           <li><a href="https://www.epa.gov/pm-pollution">Particulate Matter 2.5 (PM 2.5)</a></li>
@@ -88,6 +97,7 @@ const About = () => {
           </ul>
           <br />
           </div>
+
           <div>
           <p>
           Ozone refers to ground-level ozone. PM10 includes particles less than or equal to 10 micrometers in diameter. 
@@ -99,16 +109,16 @@ const About = () => {
           <div>
           <h1>US Air Quality Index </h1>
           <br />
+
           <p> 
           The US AQI provides a standard to measure pollution and categories to interpret health concern for humans. 
           Different countries have different AQI standards, however with similar pollutants and categories. 
           Measures from the US AQI are developed and interpreted as described below from the US EPA:
           </p> 
           <br />
-          <div className='aboutTable'>
-          <div className='aboutTh'>
-          <div className='aboutTr'>
-          <div className='aboutTd'>
+
+          <div className={aboutStyle['about-table']}>
+         <table>
           <tr>
           <th>Air Quality Index (AQI) Values</th>
           <th>Levels of Health Concern</th>
@@ -141,11 +151,10 @@ const About = () => {
           <td>301 to 500</td>
           <td>Hazardous</td>
           </tr>
-          </div>
-          </div>
-          </div>
+          </table>
           </div>
           <br />
+
           <li><strong>"Good" AQI is 0 - 50.</strong> Air quality is considered satisfactory, and air pollution poses little or no risk.</li>
           <li><strong>"Moderate" AQI is 51 - 100.</strong> Air quality is acceptable; however, for some pollutants there may be a moderate health concern for a very small
           number of people. For example, people who are unusually sensitive to ozone may experience respiratory symptoms.</li>
@@ -154,6 +163,7 @@ const About = () => {
           <li><strong>"Very Unhealthy" AQI is 201 - 300.</strong> This would trigger a health alert signifying that everyone may experience more serious health effects.</li>
           <li><strong>"Hazardous" AQI greater than 300.</strong> This would trigger health warnings of emergency conditions. The entire population is more likely to be affected.</li>
           <br />
+      
           <p> 
           Further information is available here: <a href="https://www.epa.gov/outdoor-air-quality-data/air-data-basic-information">https://www.epa.gov/outdoor-air-quality-data/air-data-basic-information</a>
           </p>
@@ -163,6 +173,7 @@ const About = () => {
           <div>
           <h1> Air Quality Measures</h1>
           <br />
+
           <p>
           Air pollutants, such as PM2.5, PM10 and Ozone, are measured via <em>micrograms per cubic meter (µg/m3)</em> or by <em>parts per million (ppm).</em> 
           Its from these measures that the US AQI categories (e.g. 0 to 50) as shown above are developed. Our analyses of long term trends in 
@@ -170,36 +181,49 @@ const About = () => {
           “moderate” air quality. 
           </p>
           <br />
-          <p>Ozone: </p>
-          <img src="ozone.jpg"/>
-          <br />
-          <p>PM2.5: </p>
-          <img src="pm25.jpg"/>
-          <br />
-          <p>PM10: </p>
-          <img src="pm10.jpg"/>
-          <br />
+          <div className={aboutStyle['ozone']}>
+          <p><strong>Ozone:</strong></p>
+          <img className="ozone" src={ozoneValue}/>
           </div>
+    
+          <br/>
+          <div className={aboutStyle['pm25']}>
+          <p><strong>PM2.5:</strong></p>
+          <img src={pm25Value}/>
+          </div>
+          <br/>
+          <div className={aboutStyle['pm10']}>
+          <p><strong>PM10:</strong></p>
+          <img src={pm10Value}/>
+          <br/>
+          <br/>
+          </div>
+          </div>
+         
 
-
-          <div>
+        <div>
           <h1> Wildfire Statistics</h1>
           <p> 
           The WAQT app provides various statistical measures of current and historical wildfires, primarily calculated from the 
           Wildland Fire Locations Full History API. The calculations are performed as explained below only for incidents which have 
           the specific information available, and for the specified US county and its state. 
           </p>
-          <br />
-          <img src="FireTable.jpg"></img>
-          <br />
+          <br />   
           </div>
+          <div className={aboutStyle['fire-table']}>
+          <img src={FireTable}></img>
+          <br />
+      
           <br/>
           <p>Further information about WAQT is available in our GitHub page. 
           </p>
           <br />
-        </p>
+        </div>
+          </p>
         </body>
-      </div>
+        </div>
+    
+  
     </>
   );
 };
